45531944796b281647cf2a2c532552f48352b4ca
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2023-12-26 19:59:19 +0300

    fix: move ProfileScreen to SafeAreaView

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -5,23 +5,26 @@ import {
   CollapsibleHeaderSectionList,
 } from "react-native-collapsible-header-views";
 import Header from "../components/Header";
+import CustomSafeAreaView from "../components/CustomSafeAreaView";
 
 const { colors, sizes } = global.config.style;
 
 const ProfileScreen = () => {
   return (
-    <CollapsibleHeaderScrollView
-      CollapsibleHeaderComponent={<Header title="Filmer" />}
-      headerHeight={60}
-      showsVerticalScrollIndicator={false}
-      statusBarHeight={Platform.OS === "ios" ? 20 : 0}
-      headerContainerBackgroundColor={colors.dark1}
-      style={styles.container}
-    >
-      <Text>ProfileScreen</Text>
+    <CustomSafeAreaView>
+      <CollapsibleHeaderScrollView
+        CollapsibleHeaderComponent={<Header title="Filmer" />}
+        headerHeight={60}
+        showsVerticalScrollIndicator={false}
+        statusBarHeight={Platform.OS === "ios" ? 20 : 0}
+        headerContainerBackgroundColor={colors.dark1}
+        style={styles.container}
+      >
+        <Text>ProfileScreen</Text>
 
-      <View style={{ marginBottom: sizes.tabbarSpace }} />
-    </CollapsibleHeaderScrollView>
+        <View style={{ marginBottom: sizes.tabbarSpace }} />
+      </CollapsibleHeaderScrollView>
+    </CustomSafeAreaView>
   );
 };
 

6b5e4a1b3938b48416ef4ba7dca01f32db75fc45
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-01-04 22:53:25 +0300

    refactor: convert app.config.js to TS

---
+++ app.config.ts
@@ -0,0 +1,36 @@
+import { ExpoConfig } from "@expo/config-types";
+
+const config: Partial<ExpoConfig> = {
+  name: "Filmer",
+  slug: "Filmer",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    package: "dev.kavakci.filmer",
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID || "default_project_id",
+    },
+  },
+};
+
+export default config;

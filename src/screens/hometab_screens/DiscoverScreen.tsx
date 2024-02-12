1ba093e81a57a6d657e26e0f5b1ad2a4a04e2566
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-02-12 18:15:16 +0300

    feat: complete movie recommendation functionality

--- src/screens/hometab_screens/DiscoverScreen.tsx
+++ src/screens/hometab_screens/DiscoverScreen.tsx
@@ -1,14 +1,20 @@
-import { View, StyleSheet, Platform, Text } from "react-native";
-import React, { useContext, useEffect } from "react";
+import { View, StyleSheet, Platform, Text, FlatList } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
 import { CollapsibleHeaderScrollView } from "react-native-collapsible-header-views";
 import Header from "../../components/Header";
 import { SimpleGrid } from "react-native-super-grid";
 import BigButton from "../../components/BigButton";
 import { MaterialIcons } from "@expo/vector-icons";
-import { colors, sizes, hexTransparencies } from "../../config";
+import { colors, sizes, hexTransparencies, genres } from "../../config";
 import TitleText from "src/components/TitleText";
-import { fetchPopularMoviesByGenre, getMovieDetails } from "src/api/tmdb";
+import {
+  Movie,
+  fetchPopularMoviesByGenre,
+  getMovieDetails,
+} from "src/api/tmdb";
 import MovieContext from "src/context/MovieContext";
+import { useFocusEffect } from "@react-navigation/native";
+import SimpleCard from "src/components/SimpleCard";
 
 interface ButtonData {
   title: string;
@@ -19,32 +25,51 @@ interface ButtonData {
 const DiscoverScreen = () => {
   const { state } = useContext(MovieContext);
   const watchedMovies = state.watchedList;
+  const [genreList, setGenreList] = useState([]);
+
   console.log(watchedMovies);
-  let genreList = [];
 
-  watchedMovies.forEach((movie) => {
-    useEffect(() => {
-      const fetchGenres = async () => {
-        try {
-          const data = await getMovieDetails(movie);
-          data.genres.forEach((genres) => {
-            if (genres.hasOwnProperty("id")) {
-              genreList.push(genres.id);
-            }
-          });
-          console.log(genreList);
-        } catch (error) {
-          console.error("Error while retrieving genre data: ", error);
-        }
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const movieDetailsPromises = watchedMovies.map((movie) =>
+          getMovieDetails(movie)
+        );
+        const movieDetails = await Promise.all(movieDetailsPromises);
 
-      fetchGenres();
-    }, []);
-  });
+        const genres = movieDetails.flatMap((movie) => movie.genres);
+        const filteredGenres = genres
+          .filter((genre) => genre.hasOwnProperty("id"))
+          .map((genre) => genre.id);
 
-  genreList.forEach((value, index) => {
-    useEffect(() => {});
-  });
+        const counts = {};
+        filteredGenres.forEach((item) => {
+          counts[item] = (counts[item] || 0) + 1;
+        });
+
+        const genreCountArray = Object.keys(counts).map((genre) => ({
+          genre,
+          count: counts[genre],
+        }));
+
+        genreCountArray.sort((a, b) => b.count - a.count);
+
+        const sortedGenreList = genreCountArray.map((item) => item.genre);
+
+        setGenreList(sortedGenreList);
+      } catch (error) {
+        console.error("Error while retrieving genre data: ", error);
+      }
+    };
+
+    if (watchedMovies.length > 0) {
+      fetchData();
+    }
+  }, [watchedMovies]);
+
+  useEffect(() => {
+    console.log("Genre List:", genreList);
+  }, [genreList]);
 
   const DiscoverScreenButtons = () => {
     const buttons: ButtonData[] = [
@@ -76,20 +101,74 @@ const DiscoverScreen = () => {
   };
 
   const RecommendedByGenre = ({ genre }) => {
+    const [moviesByGenre, setMoviesByGenre] = useState([]);
+
+    const shuffle = (array) => {
+      let currentIndex = array.length,
+        randomIndex;
+
+      // While there remain elements to shuffle.
+      while (currentIndex > 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+        ];
+      }
+
+      return array;
+    };
+
+    useEffect(() => {
+      const popular = async () => {
+        try {
+          const data = await fetchPopularMoviesByGenre(genre);
+          setMoviesByGenre(shuffle(data.results));
+        } catch (error) {
+          console.error("Error while retrieving popular movies data: ", error);
+        }
+      };
+
+      popular();
+    }, []);
+
     return (
       <View style={recommendedByGenre.container}>
         <TitleText
-          text={`Because you like ${genre}`}
+          text={`Because you like ${genres[genre]}`}
           style={recommendedByGenre.title}
+        />
+        <FlatList
+          data={moviesByGenre}
+          keyExtractor={(item) => item.id.toString()}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+            <SimpleCard key={index} id={item.id} />
+          )}
         />
       </View>
     );
   };
 
-  const components = [
-    <DiscoverScreenButtons />,
-    <RecommendedByGenre genre="Drama" />,
-  ];
+  const RecommendedByGenreList = () => {
+    return (
+      <View>
+        {genreList.slice(0, 3).map((genre, index) => (
+          <View key={index}>
+            <RecommendedByGenre genre={genre} />
+            {index !== genreList.length - 1 && <View style={{ margin: 6 }} />}
+          </View>
+        ))}
+      </View>
+    );
+  };
+
+  const components = [<DiscoverScreenButtons />, <RecommendedByGenreList />];
 
   return (
     <CollapsibleHeaderScrollView
@@ -102,11 +181,13 @@ const DiscoverScreen = () => {
     >
       {components.map((item, index) => {
         return (
-          <View key={index} style={{ marginBottom: 10 }}>
+          <View key={index} style={{ marginBottom: 15 }}>
             {item}
           </View>
         );
       })}
+
+      <View style={{ marginBottom: sizes.tabbarSpace }} />
     </CollapsibleHeaderScrollView>
   );
 };
@@ -124,6 +205,7 @@ const recommendedByGenre = StyleSheet.create({
   },
   title: {
     fontSize: 22,
+    marginBottom: 15,
   },
 });
 

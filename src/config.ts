00777a19ae95c4f664d7402cd1acf241ec2922eb
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-02-12 18:14:26 +0300

    feat: add movie genre ids and names to config

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,14 @@ interface HexTransparencies {
   [key: number]: string;
 }
 
+interface Genres {
+  [key: number]: string;
+}
+
 interface Sizes {
   radiusBig: number;
   radius: number;
+  radiusSmall: number;
   tabbarSpace: number;
 }
 
@@ -36,7 +41,30 @@ export const colors: Colors = {
   light_cyan: "#92d3cc",
 };
 
+export const genres: Genres = {
+  28: "Action",
+  12: "Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  14: "Fantasy",
+  36: "History",
+  27: "Horror",
+  10402: "Music",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Science Fiction",
+  10770: "TV Movie",
+  53: "Thriller",
+  10752: "War",
+  37: "Western",
+};
+
 export const sizes: Sizes = {
+  radiusSmall: 17,
   radiusBig: 25,
   radius: 20,
   tabbarSpace: 70,
